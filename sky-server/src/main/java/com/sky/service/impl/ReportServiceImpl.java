3689519e2c0b97f4defa2539af14ee8f354e3f5b
leo59123 <2442595123@qq.com>
2023-10-27 15:22:46 +0800

    <feat>:< finish Statistic on user >

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,13 +2,17 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
 
+import com.sky.vo.UserReportVO;
+import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -25,6 +29,9 @@ public class ReportServiceImpl implements ReportService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private UserMapper userMapper;
+
     /**
      * 统计指定时间区间内的营业额
      * @param begin
@@ -35,6 +42,7 @@ public class ReportServiceImpl implements ReportService {
 
         //根据开始结束日期拼凑出x轴:begin~end之间的每天日期
         List<LocalDate> dateList= new ArrayList<>();
+        dateList.add(begin);
         while(!begin.equals(end)){
             begin=begin.plusDays(1);
             dateList.add(begin);
@@ -66,4 +74,55 @@ public class ReportServiceImpl implements ReportService {
                 .dateList(StringUtil.join(",", dateList))//将集合的元素取出来,转换为字符串,拼起来,用,分割
                 .build();
     }
+
+    /**
+     * 统计指定时间范围内的用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        //核心目标是拼出VO
+
+        //得到x轴:
+        List<LocalDate> dateList= new ArrayList<>();
+        dateList.add(begin);
+        while(!begin.equals(end)){
+            begin=begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+
+        //统计每天新增用户量 :两边限制为一天的最大值和最小值
+        List<Integer> newUserList =new ArrayList<>();
+        //select COUNT(id) from user where crete_time> start and crete_time < ?
+
+        //统计每天总用户的数量: 只限制右边为 end的最小值
+        List<Integer> totalUserList =new ArrayList<>();
+        //select COUNT(id) from user where crete_time < ?
+
+        for(LocalDate date:dateList){
+            //先得到begin和end
+            LocalDateTime startOfDate = LocalDateTime.of(date, LocalTime.MIN) ;// 给出date以及具体的时分秒
+            LocalDateTime endOfDate = LocalDateTime.of(date,LocalTime.MAX);
+            Map map=new HashMap();
+            map.put("end",endOfDate);
+            //总用户数量
+            Integer totalUser = userMapper.countByMap(map);
+            map.put("begin",startOfDate);
+            //新增用户数量
+            Integer newUser = userMapper.countByMap(map);
+
+            totalUserList.add(totalUser);
+            newUserList.add(newUser);
+        }
+
+        return UserReportVO.builder()
+                .dateList(StringUtil.join(",",dateList))
+                .newUserList(StringUtil.join(",",newUserList))
+                .totalUserList(StringUtil.join(",",totalUserList))
+                .build();
+    }
+
+
 }
